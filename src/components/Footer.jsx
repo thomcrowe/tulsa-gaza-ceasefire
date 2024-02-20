d1cf4de83b6e9ce21fe69aa2ccb6648eb4bf58c0
Thom Crowe <thom@thomcrowe.com>
2024-02-19 22:49:29 -0600

    fixing lint error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export function Footer() {
       <Container className="flex flex-col items-center justify-between md:flex-row">
         <Logo className="h-12 w-auto text-slate-900" />
         <p className="mt-6 text-base text-slate-500 md:mt-0">
-          Copyright &copy; {new Date().getFullYear()} Tulsa's Gaza Ceasefire Pilgrimage
+          Copyright &copy; {new Date().getFullYear()} Tulsa&apos;s Gaza Ceasefire Pilgrimage
         </p>
       </Container>
     </footer>
